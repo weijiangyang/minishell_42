9d713086782f17cb6428beb5cce7f729290a9004
Weijiang Yang <weiyang@f4r12s16.clusters.42paris.fr>
2026-01-05 15:05:33 +0100

    add ft_strcmp to libft

--- libft/libft.h
+++ libft/libft.h
@@ -71,5 +71,6 @@ void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
 						void (*del)(void *));
+int 				ft_strcmp(const char *s1, const char *s2);
 
 #endif
--- src/parse/utils.c
+++ src/parse/utils.c
@@ -151,13 +151,5 @@ char *safe_strdup(const char *s)
     return p;
 }
 
-int ft_strcmp(const char *s1, const char *s2)
-{
-    while (*s1 && (*s1 == *s2))
-    {
-        s1++;
-        s2++;
-    }
-    return ((unsigned char)*s1 - (unsigned char)*s2);
-}
+
 
