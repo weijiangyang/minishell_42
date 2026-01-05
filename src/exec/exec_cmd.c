02a977efcdbeaa8f39f06ed2ca64b431df10683c
Weijiang Yang <weiyang@f4r12s16.clusters.42paris.fr>
2026-01-05 15:10:01 +0100

    exit parent

--- src/exec/exec_cmd.c
+++ src/exec/exec_cmd.c
@@ -163,12 +163,12 @@ int exec_cmd_node(t_minishell *msh, ast *node, int in_fd, int out_fd)
 	}
 	if (!node->argv || !node->argv[0])
 		return (run_redir_only_parent(msh, node, in_fd, out_fd));
-	/*if (is_builtin_parent(node->argv[0]))
+	if (is_builtin_parent(node->argv[0]))
 	{
 		ret = run_builtin_parent_logic(msh, node, in_fd, out_fd);
 		msh->last_exit_status = ret;
 		return (ret);
-	}*/
+	}
 	ensure_paths_ready(msh);
 	return (run_external_wait(msh, node, in_fd, out_fd));
 }
