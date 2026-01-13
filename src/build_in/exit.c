02c29bb12bbd53134c14c7229bb2f6cd68d60d32
Yue Zhang <yzhang2@f1br7s12.clusters.42paris.fr>
2026-01-13 15:30:42 +0100

    exit 信息

--- src/build_in/exit.c
+++ src/build_in/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: weiyang <weiyang@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yzhang2 <yzhang2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 13:24:07 by weiyang           #+#    #+#             */
-/*   Updated: 2026/01/11 20:31:46 by weiyang          ###   ########.fr       */
+/*   Updated: 2026/01/13 15:29:14 by yzhang2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,14 +65,44 @@ static void	set_exit_state(t_minishell *msh, int ret)
 		msh->exit_code = ret;
 		msh->should_exit = 1;
 	}
-	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO))
-		write(STDOUT_FILENO, "exit\n", 5);
 }
+
+// int	builtin_exit(char **argv, t_minishell *msh)
+// {
+// 	int	ret;
+
+// 	ft_putendl_fd("exit", 2);
+// 	ret = 0;
+// 	if (argv && argv[1])
+// 	{
+// 		if (!is_numeric(argv[1]))
+// 		{
+// 			ms_put3("minishell: exit: ", argv[1],
+// 				": numeric argument required\n");
+// 			ret = 2;
+// 		}
+// 		else if (argv[2])
+// 		{
+// 			ms_put3("minishell: exit: ", "too many arguments", "\n");
+// 			if (msh)
+// 				msh->last_exit_status = 1;
+// 			return (1);
+// 		}
+// 		else
+// 			ret = (unsigned char)to_long(argv[1]);
+// 	}
+// 	else if (msh)
+// 		ret = msh->last_exit_status;
+// 	return (set_exit_state(msh, ret), ret);
+// }
+/* src/build_in/exit.c */
 
 int	builtin_exit(char **argv, t_minishell *msh)
 {
 	int	ret;
 
+	if (isatty(0))
+		ft_putendl_fd("exit", 2);
 	ret = 0;
 	if (argv && argv[1])
 	{
@@ -80,20 +110,18 @@ int	builtin_exit(char **argv, t_minishell *msh)
 		{
 			ms_put3("minishell: exit: ", argv[1],
 				": numeric argument required\n");
-			ret = 2;
+			return (set_exit_state(msh, 2), 2);
 		}
-		else if (argv[2])
+		if (argv[2])
 		{
 			ms_put3("minishell: exit: ", "too many arguments", "\n");
 			if (msh)
 				msh->last_exit_status = 1;
 			return (1);
 		}
-		else
-			ret = (unsigned char)to_long(argv[1]);
+		ret = (unsigned char)to_long(argv[1]);
 	}
 	else if (msh)
 		ret = msh->last_exit_status;
-	set_exit_state(msh, ret);
-	return (ret);
+	return (set_exit_state(msh, ret), ret);
 }
